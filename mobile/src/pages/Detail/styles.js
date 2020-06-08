66c9186731e2adffa9c35b9c9f2254723f956117
Davi Pessoa <davialvespessoa@gmail.com>
2020-06-08 11:14:49 -0300

    Aplicação completa(back, front e mobile)

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,78 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom: 16,
+        marginTop: 40,
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        marginTop: 24,
+        fontWeight: 'bold',
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        color: '#737380',
+    },
+
+    contactBox: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom: 16,
+    },
+
+    heroTitle: {
+        fontWeight: 'bold',
+        fontSize: 19,
+        color: '#13131a',
+        lineHeight: 30,
+    },
+
+    heroDescription: {
+        fontSize: 15,
+        color: '#737380',
+        marginTop: 16,
+    },
+
+    actions: {
+        flexDirection: 'row',
+        marginTop: 16,
+        justifyContent: 'space-between',
+    },
+
+    action: {
+        backgroundColor: '#e02041',
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width: '48%',
+    },
+
+    actionText: {
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold',
+    }
+});
